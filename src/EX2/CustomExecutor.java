25eae7000f25093c2c605852140bc7550a8d07f3
Dor Yanay <dor3518@gmail.com>
2023-01-09 18:14:52 +0200

    :TASK:

--- src/EX2/CustomExecutor.java
+++ src/EX2/CustomExecutor.java
@@ -3,12 +3,13 @@ package EX2;
 import java.util.PriorityQueue;
 import java.util.concurrent.PriorityBlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 public class CustomExecutor extends ThreadPoolExecutor {
-    private PriorityBlockingQueue<Task> Qt;
-    private ThreadPoolExecutor exec;
-    public CustomExecutor () {
-
-
+    private static int numOfCores = Runtime.getRuntime().availableProcessors();
+    private static PriorityBlockingQueue Qt;
+    public CustomExecutor() {
+        super(numOfCores/2, numOfCores-1, 300, TimeUnit.MILLISECONDS,
+                Qt = new PriorityBlockingQueue<>());
     }
 }
--- src/EX2/Task.java
+++ src/EX2/Task.java
@@ -1,31 +1,62 @@
 package EX2;
 
+import java.util.Objects;
 import java.util.concurrent.Callable;
 import java.util.concurrent.FutureTask;
 
-public class Task<T> extends FutureTask<T> implements Callable<T>, Comparable<Task>  {
-    private TaskType tasktype;
+public class Task<T> extends FutureTask<T> implements Callable<T>, Comparable<Task<T>>  {
+    private TaskType type;
     private Callable<T> cable;
-
-    public Task(Callable<T> callable) {
-        super(callable);
+    private Task(Callable<T> cable) {
+        this(TaskType.OTHER, cable);
     }
-
-
+    private Task(TaskType type, Callable<T> cable) {
+        super(cable);
+        this.cable = cable;
+        this.type = type;
+    }
     public TaskType getType() {
-    return tasktype;
+    return type;
     }
     public void setType(TaskType tasktype) {
-    this.tasktype=tasktype;
+    this.type=type;
     }
 
+    public Callable<T> getCable() {
+        return cable;
+    }
+
+    public void setCable(Callable<T> cable) {
+        this.cable = cable;
+    }
+
+    public boolean equals(Task<T> type) {
+        if(this.compareTo(type)==0) {
+            return true;
+        }
+        return false;
+    }
+    @Override
+    public int hashCode() {
+        return Objects.hash(type, cable);
+    }
     @Override
     public T call() throws Exception {
         return cable.call();
     }
-
     @Override
-    public int compareTo(Task o) {
-        return 1;
+    public int compareTo(Task other) {
+        if(this.type.getPriorityValue()>other.type.getPriorityValue())
+            return 1;
+        if(this.type.getPriorityValue()<other.type.getPriorityValue())
+            return -1;
+        return 0;
     }
+    public static <T>Task<T> createTask(Callable<T> cable) {
+        return new Task<T>(cable);
+    }
+    public static <T>Task<T> createTask(Callable<T> cable , TaskType type) {
+        return new Task<T>(type,cable);
+    }
+
 }
